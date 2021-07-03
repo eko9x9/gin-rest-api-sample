7f0031e1d1b2c5d112bbb8d3a491434234470a5b
eko969 <ekonurrohman969@gmail.com>
2021-07-03 21:29:21 +0700

    chore: update

--- app/controllers/auth.controller.go
+++ app/controllers/auth.controller.go
@@ -1,7 +1,11 @@
 package controllers
 
 import (
+	"net/http"
+
+	"github.com/eko9x9/gin-rest-api-sample/app/form"
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 )
 
 type AuthController struct {
@@ -10,6 +14,21 @@ type AuthController struct {
 func (ctrl *AuthController) Register() gin.HandlerFunc {
 
 	return func(c *gin.Context) {
+		body := form.RegisterRequestBody{}
+
+		if err := c.ShouldBindJSON(body); err != nil {
+			validate := validator.New()
+
+			if err := validate.Struct(body); err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{
+					"error": err.Error(),
+				})
+
+				c.Abort()
+
+				return
+			}
+		}
 
 	}
 }
