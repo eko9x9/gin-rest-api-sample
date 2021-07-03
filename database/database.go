5bb6787bac8a84f525a95ac6f4429076fa2f58f9
eko969 <ekonurrohman969@gmail.com>
2021-07-03 21:47:28 +0700

    feat: add config database using env

--- database/database.go
+++ database/database.go
@@ -1,13 +1,19 @@
 package database
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/eko9x9/gin-rest-api-sample/database/migrations"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func Initialize() (*gorm.DB, error) {
-	dsn := "host=postgres user=postgres password=postgres dbname=sample_api port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	conf := fmt.Sprintf("host=%s user=%s password=%v dbname=%v port=%v sslmode=disable TimeZone=Asia/Jakarta",
+		os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
+
+	dsn := conf
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	migrations.Migrate(db)
 	return db, err
