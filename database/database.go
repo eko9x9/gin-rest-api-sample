eabcf2b9a66088052acd073a1914ccd4e75c302a
eko969 <ekonurrohman969@gmail.com>
2021-07-03 21:29:55 +0700

    Added database connection

--- database/database.go
+++ database/database.go
@@ -1,1 +1,14 @@
 package database
+
+import (
+	"github.com/eko9x9/gin-rest-api-sample/database/migrations"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func Initialize() (*gorm.DB, error) {
+	dsn := "host=postgres user=postgres password=postgres dbname=sample_api port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	migrations.Migrate(db)
+	return db, err
+}
