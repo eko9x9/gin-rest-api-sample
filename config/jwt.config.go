2711afcd1b1ce48b95d93b4efa4be12cfd2a09d9
eko969 <ekonurrohman969@gmail.com>
2021-07-04 19:39:37 +0700

    feat: config jwt key

---
+++ config/jwt.config.go
@@ -0,0 +1,15 @@
+package config
+
+import (
+	"io/ioutil"
+	"os"
+)
+
+func GetKey() ([]byte, error) {
+	pwd, _ := os.Getwd()
+
+	keyPath := pwd + "/config/secret/jwt.key"
+	key, err := ioutil.ReadFile(keyPath)
+
+	return key, err
+}
